2b1b83b1f2b14dff4a18fcaefbcd51e99adacf00
Dataxis-UditSingh <udit.singh@dataxis.io>
2026-05-19 11:53:03 +0530

    Update design

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="h-screen flex items-center justify-center">
+      <div className="h-screen flex items-center justify-center bg-black">
         <h1 className="text-emerald-400 text-5xl font-semibold text-center">
           Testing project
         </h1>
