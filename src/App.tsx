c16100bc065915d02b6a34160725f213806dce7d
Dataxis-UditSingh <udit.singh@dataxis.io>
2026-05-20 10:07:17 +0530

    Testing CD

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="h-screen flex items-center justify-center bg-black">
-        <h1 className="text-emerald-400 text-5xl font-semibold text-center">
+        <h1 className="text-pink-400 text-5xl font-semibold text-center">
           Testing project
         </h1>
       </div>
