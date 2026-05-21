66f3d86762fbc06aad6f7b691ed3a93432736aab
Dataxis-UditSingh <udit.singh@dataxis.io>
2026-05-21 16:09:24 +0530

    Update chnages for pipelines

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="h-screen flex items-center justify-center bg-black">
-        <h1 className="text-pink-400 text-5xl font-semibold text-center">
+      <div className="h-screen flex items-center justify-center flex-col bg-gray-500">
+        <h1 className="text-blue-800 text-5xl font-semibold text-center">
           Testing project
         </h1>
+        <p className="text-white text-lg text-center mt-4">
+          This was Testing project for checking the pipeline and GitOps are setup successfully.
+        </p>
       </div>
     </>
   );
