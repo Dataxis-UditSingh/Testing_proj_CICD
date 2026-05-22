22b85f335136dc661ecda2e804094be17c2b6bb5
Dataxis-UditSingh <udit.singh@dataxis.io>
2026-05-22 15:48:25 +0530

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div className="h-screen flex items-center justify-center flex-col bg-gray-500">
         <h1 className="text-blue-800 text-5xl font-semibold text-center">
-          Testing project
+          Testing project for CICD
         </h1>
         <p className="text-white text-lg text-center mt-4">
           This was Testing project for checking the pipeline and GitOps are setup successfully.
