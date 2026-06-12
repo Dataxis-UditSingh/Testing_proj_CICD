f420b5235ba389e7a80415737785ec079b3f7b06
Dataxis-UditSingh <udit.singh@dataxis.io>
2026-06-12 22:04:49 +0530

    Update Version2

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,12 @@ function App() {
         <div className="w-96 h-96 mb-8 rounded-full overflow-hidden">
           <img src="https://www.spinny.com/blog/wp-content/uploads/2024/09/videoframe_0.webp" alt="Car_Image" className="h-full w-full object-cover" />
         </div>
-        <h1 className="text-pink-800 text-5xl font-semibold text-center">
-          Testing project for CICD
-        </h1>
-
         <p className="text-white text-3xl text-center mt-4">
           This was Testing project for CICD Pipeline chekup.
         </p>
-
+        <h1 className="text-pink-800 text-5xl font-semibold text-center">
+          Testing project for CICD
+        </h1>
         <p className="text-pink-600 text-lg text-center mt-4">
           This was Testing project for Image Automation.
         </p>
