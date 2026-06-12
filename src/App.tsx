b6d1dec7905954ce067812f422c99c47b560a01e
Dataxis-UditSingh <udit.singh@dataxis.io>
2026-06-12 21:21:03 +0530

    Insert Image

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <h1 className="text-pink-800 text-5xl font-semibold text-center">
           Testing project for CICD
         </h1>
-        
+
         <p className="text-white text-3xl text-center mt-4">
           This was Testing project for CICD Pipeline chekup.
         </p>
