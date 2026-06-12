79ed8dc922c9d200bdc7ae9c16ce989c887c7882
Dataxis-UditSingh <udit.singh@dataxis.io>
2026-06-12 22:49:12 +0530

    Update image border

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="h-screen flex items-center justify-center flex-col bg-black">
-        <div className="w-96 h-96 rounded-full overflow-hidden">
+        <div className="w-96 h-96 rounded-2xl overflow-hidden">
           <img src="https://www.spinny.com/blog/wp-content/uploads/2024/09/videoframe_0.webp" alt="Car_Image" className="h-full w-full object-cover" />
         </div>
         <p className="text-white text-3xl text-center pt-5">
